98a8c1e18f31966061876187a0471bb729b0a862
oo10 <simple11@vip.qq.com>
2016-05-20 01:25:33 +0800

    bug fixed

--- exp/app/js/controllers.js
+++ exp/app/js/controllers.js
@@ -4,11 +4,29 @@ appCtrls.controller('HelloCtrl', ['$scope','$http',
     function($scope) {
         $scope.boxs = [{
             title: '去食堂吃饭'
-        },{
+        }, {
             title: '吃外卖'
-        },{
+        }, {
             title: '去吃黄焖鸡'
         }];
+        var names = [{
+            title: '去食堂吃饭111'
+        }, {
+            title: '吃外卖'
+        }, {
+            title: '去吃黄焖鸡'
+        }];
+        localStorage['names'] = JSON.stringify(names);
+        var namess = JSON.parse(localStorage['names']);
+        console.log(namess);
+
+        var randomNumber = Math.floor((Math.random() * $scope.boxs.length));
+        var randomNumber2 = Math.floor((Math.random() * localStorage.length));
+        //$scope.boxTitleRodon = $scope.boxs[randomNumber].title;    //result
+        $scope.titleRodon = "吃外卖";//localStorage.getItem("test" + randomNumber2);    //result
+        console.log($scope.boxs);
+        console.log("aa+" + randomNumber2);
+        console.log("bb+" + localStorage.getItem("test" + randomNumber2));
 
         //$http.get('http://localhost:63342/nhpop.cn/angular.js/exp/app/boxs.json').success(function(data){
         //    $scope.boxs = data.boxs;
@@ -16,25 +34,25 @@ appCtrls.controller('HelloCtrl', ['$scope','$http',
         //}).error(function(){
         //    alert("an unexpected error ocurred!");
         //});
-        localStorage.setItem("test1","去食堂吃饭");
-        localStorage.setItem("test2","吃外卖");
-        localStorage.setItem("test3","去吃黄焖鸡");
+        localStorage.setItem("test1", "去食堂吃饭");
+        localStorage.setItem("test2", "吃外卖");
+        localStorage.setItem("test3", "去吃黄焖鸡");
         var test = localStorage.test1;
         console.log(test);
 
 
+        $scope.pageClass = "hello";
 
-        $scope.pageClass="hello";
-
-        $scope.alert = function(){
+        $scope.alert = function () {
             $scope.showDialog = "translateY(-50%) scale(1)";
         };
 
-        $scope.del = function(idx){
-            $scope.boxs.splice(idx,1);
+        $scope.del = function (idx) {
+            $scope.boxs.splice(idx, 1);
+            localStorage.removeItem("test" + (idx + 1));
         };
         var i = 3;
-        $scope.add = function(idx) {
+        $scope.add = function (idx) {
             //var boxs = {
             //    "title": $scope.addtitle
             //};
@@ -45,36 +63,31 @@ appCtrls.controller('HelloCtrl', ['$scope','$http',
             //    alert("failure message:" + JSON.stringify({data:data}));
             //});
             i++;
-            localStorage.setItem("test"+(i),$scope.addtitle);
-            $scope.boxs.push({"title" : $scope.addtitle});
+            localStorage.setItem("test" + i, $scope.addtitle);
+            $scope.boxs.push({"title": $scope.addtitle});
             console.log($scope.boxs);
-            console.log(i+":"+localStorage.getItem("test"+(i)));
-            //localStorage.removeItem(key) 删除
-            for(var a = 0, len = localStorage.length;a<len;a++){
-                var getKey=localStorage.key(i);
-                if(getKey.substring(0,4)=="test"){
-                    //从0开始，长度为4的子字符串是否为test
-                    //然后这里再取出值
-                    console.log($scope.boxs);
-                }
+            console.log(i + ":" + localStorage.getItem("test" + (i)));
+            for (var a = 1, len = localStorage.length; a < len + 1; a++) {
+                var getVal = localStorage.getItem("test" + (a));
+                console.log(getVal);
             }
 
             $scope.showDialog = "translateY(-50%) scale(0)";
         };
 
-        $scope.cancel = function(){
+        $scope.cancel = function () {
             $scope.showDialog = "translateY(-50%) scale(0)";
         };
 
-        var randomNumber = Math.floor((Math.random()*$scope.boxs.length));
-        $scope.boxTitleRodon = $scope.boxs[randomNumber].title;    //result
-        console.log($scope.boxs);
 
-        $scope.refresh = function(){
+        $scope.refresh = function () {
             console.log($scope.boxs);
-            alert("0k");
-            $scope.boxTitleRodon = $scope.boxs[0].title;    //result
-        };
+            //$scope.boxTitleRodon = $scope.boxs[0].title;    //result
+            var randomNumber2 = Math.ceil((Math.random() * (localStorage.length-1)));
+            var result = localStorage.getItem("test" + randomNumber2);
+            $scope.boxTitleRodon = result;   //result
+            console.log(result);
+        }
     }
 ]);
 
